b76c2d65c0a50f302ac9b68775b088f146ac4cc6
tdelgado37 <tdelgado37.td@gmail.com>
2018-03-19 18:18:55 -0400

    Adjusted main menu page

--- app/src/main/java/com/example/nicholas/pathfinderapp/MainMenuActivity.java
+++ app/src/main/java/com/example/nicholas/pathfinderapp/MainMenuActivity.java
@@ -1,6 +1,7 @@
 package com.example.nicholas.pathfinderapp;
 
 import android.content.Intent;
+import android.net.Uri;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
@@ -8,14 +9,14 @@ import android.widget.Button;
 
 public class MainMenuActivity extends AppCompatActivity {
 
-    private Button btnProfile, btnSettings, btnMap;
+    private Button btnProfile, btnSettings, btnGenRoute;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main_menu);
 
-        btnMap = (Button) findViewById(R.id.map_button);
+        btnGenRoute = (Button) findViewById(R.id.GenRoute_button);
         btnProfile = (Button) findViewById(R.id.profile_button);
         btnSettings = (Button) findViewById(R.id.settings_button);
 
@@ -24,9 +25,34 @@ public class MainMenuActivity extends AppCompatActivity {
             public void onClick(View v) {
                 Intent intent = new Intent(MainMenuActivity.this, MainActivity.class);
                 startActivity(intent);
-                finish();
             }
         });
+
+        btnGenRoute.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                /*Intent intent = new Intent(MainMenuActivity.this, MapActivity.class);
+                startActivity(intent);
+                finish();*/
+
+                Uri gmmIntentUri = Uri.parse("https://www.google.com/maps/dir/?api=1&origin=Paris,France&destination=Cherbourg,France&travelmode=driving&waypoints=Versailles,France%7CChartres,France%7CLe+Mans,France%7CCaen,France");
+                Intent mapIntent = new Intent(Intent.ACTION_VIEW);
+                mapIntent.setData(gmmIntentUri);
+                startActivity(mapIntent);
+
+            }
+        });
+
+
+        /*btnSettings.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(MainMenuActivity.this, SettingsActivity.class);
+                startActivity(intent);
+                finish();
+            }
+        });*/
+
 
 
     }
