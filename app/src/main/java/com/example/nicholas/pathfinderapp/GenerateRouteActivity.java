a78997850e6d3c9c6fa189c5cb5e02c3f389d87b
nonamenic <nonamenic@yahoo.com>
2018-03-26 15:57:47 -0400

    updates for pulling location and route logic

--- app/src/main/java/com/example/nicholas/pathfinderapp/GenerateRouteActivity.java
+++ app/src/main/java/com/example/nicholas/pathfinderapp/GenerateRouteActivity.java
@@ -2,6 +2,10 @@ package com.example.nicholas.pathfinderapp;
 
 import android.content.Context;
 import android.content.Intent;
+import android.content.pm.PackageManager;
+import android.location.Location;
+import android.location.LocationListener;
+import android.location.LocationManager;
 import android.net.Uri;
 import android.os.SystemClock;
 import android.support.v7.app.AppCompatActivity;
@@ -12,24 +16,45 @@ import android.view.View;
 import android.widget.Button;
 import android.widget.Chronometer;
 import android.widget.EditText;
+import android.widget.TextView;
 import android.widget.Toast;
 
 import com.google.firebase.database.DatabaseReference;
 import com.google.firebase.database.FirebaseDatabase;
 
-public class GenerateRouteActivity extends AppCompatActivity {
+public abstract class GenerateRouteActivity extends AppCompatActivity implements LocationListener {
 
     private Button btnStart, btnStop, btnSmallRun, btnMediumRun, btnLongRun;
+    protected LocationManager locationManager;
+    protected LocationListener locationListener;
     private Chronometer stopWatch;
     private int mLowDistance, mHighDistance;
     private DatabaseReference runnerDataBase;
     private Controller mController = Controller.getSoleInstance();
     private final String TAG = "GenerateRouteActivity";
+    public static double lat;
+    public static double lng;
+    GenerateRouteClass cls = new GenerateRouteClass();
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_generate_route);
+
+        locationManager = (LocationManager) getSystemService(Context.LOCATION_SERVICE);
+        if(checkLocationPermission()) {
+            locationManager.requestLocationUpdates(LocationManager.GPS_PROVIDER, 0, 0, this);
+            btnSmallRun.setEnabled(true);
+            btnMediumRun.setEnabled(true);
+            cls.genRouteMethod(lat, lng);
+        }
+        else{
+            Toast toast = Toast.makeText(getApplicationContext(),"ERROR: No location permission given", Toast.LENGTH_LONG);
+            toast.setGravity(Gravity.BOTTOM,0,0);
+            toast.show();
+            btnMediumRun.setEnabled(false);
+            btnSmallRun.setEnabled(false);
+        }
 
         runnerDataBase = FirebaseDatabase.getInstance().getReference();
        // Log.d(TAG, "Updating question text", new Exception());
@@ -39,6 +64,7 @@ public class GenerateRouteActivity extends AppCompatActivity {
         btnMediumRun = (Button) findViewById(R.id.mediumRun_button);
         btnLongRun = (Button) findViewById(R.id.longRun_button);
         stopWatch = (Chronometer) findViewById(R.id.stop_watch);
+
 
 
         btnSmallRun.setOnClickListener(new View.OnClickListener() {
@@ -56,16 +82,6 @@ public class GenerateRouteActivity extends AppCompatActivity {
             public void onClick(View v) {
                 Toast toast = Toast.makeText(getApplicationContext(), "You selected a medium run!", Toast.LENGTH_LONG);
                 mLowDistance = 5;
-                toast.setGravity(Gravity.BOTTOM, 0, 0);
-                toast.show();
-            }
-        });
-
-        btnLongRun.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                Toast toast = Toast.makeText(getApplicationContext(), "You selected a long run!", Toast.LENGTH_LONG);
-                mLowDistance = 11;
                 toast.setGravity(Gravity.BOTTOM, 0, 0);
                 toast.show();
             }
@@ -97,6 +113,18 @@ public class GenerateRouteActivity extends AppCompatActivity {
 
 
 
+    }
+    public boolean checkLocationPermission() {
+        String permission = "android.permission.ACCESS_FINE_LOCATION";
+        int res = this.checkCallingOrSelfPermission(permission);
+        return (res == PackageManager.PERMISSION_GRANTED);
+    }
+
+    @Override
+    public void onLocationChanged(Location location) {
+        lat = location.getLatitude();
+        lng = location.getLongitude();
+        Log.d("location change:", lat +" " + lng );
     }
 
 
